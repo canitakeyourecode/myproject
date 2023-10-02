9493f68c1099f9d0c62927fde54488c33f2f910e
yakuza <fullmetalbrain@gmail.com>
2023-10-02 13:26:51 +0300

    addProject

---
+++ MyProject/MyProject.h
@@ -0,0 +1,18 @@
+//
+//  MyProject.h
+//  MyProject
+//
+//  Created by yakuza on 02.10.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyProject.
+FOUNDATION_EXPORT double MyProjectVersionNumber;
+
+//! Project version string for MyProject.
+FOUNDATION_EXPORT const unsigned char MyProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyProject/PublicHeader.h>
+
+
